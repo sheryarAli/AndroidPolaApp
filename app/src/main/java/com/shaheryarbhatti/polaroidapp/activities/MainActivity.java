d2ca073e3de8c73a57e1e45ad93d7bdd92411f73
shery <shery_bhatti@github.com>
2017-12-17 12:08:34 +0500

    Commit 1.2

--- app/src/main/java/com/shaheryarbhatti/polaroidapp/activities/MainActivity.java
+++ app/src/main/java/com/shaheryarbhatti/polaroidapp/activities/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements DashboardFragment
     private ViewPager viewPager;
     private Toolbar toolbar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
