5e3a0f8e15995dcfe2856dd1bea5ff23f65d2d2b
shahxor99 <shahxoreali@gmail.com>
2017-12-17 11:24:25 +0500

    new layouts created

--- app/src/main/java/com/shaheryarbhatti/polaroidapp/activities/MainActivity.java
+++ app/src/main/java/com/shaheryarbhatti/polaroidapp/activities/MainActivity.java
@@ -1,20 +1,25 @@
 package com.shaheryarbhatti.polaroidapp.activities;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.shaheryarbhatti.polaroidapp.R;
 import com.shaheryarbhatti.polaroidapp.fragments.DashboardFragment;
 import com.shaheryarbhatti.polaroidapp.fragments.FeaturedFragment;
 import com.shaheryarbhatti.polaroidapp.fragments.PopularFragment;
 import com.shaheryarbhatti.polaroidapp.viewpagers.MainFragmentPagerAdapter;
-import com.shaheryarbhatti.polaroidapp.R;
 
 public class MainActivity extends AppCompatActivity implements DashboardFragment.OnFragmentInteractionListener
         , FeaturedFragment.OnFragmentInteractionListener, PopularFragment.OnFragmentInteractionListener {
@@ -28,13 +33,28 @@ public class MainActivity extends AppCompatActivity implements DashboardFragment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Intent intent = new Intent(this, ItemSelelectedActivity.class);
+//        this.startActivity(intent);
+
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#0971B8")));
+        getSupportActionBar().setElevation(0);
 
         tabs = (TabLayout) findViewById(R.id.tabs);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
-
+        View root = tabs.getChildAt(0);
+        if (root instanceof LinearLayout) {
+            ((LinearLayout) root).setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+            GradientDrawable drawable = new GradientDrawable();
+            drawable.setColor(getResources().getColor(R.color.colorPrimary));
+            drawable.setSize(2, 1);
+            ((LinearLayout) root).setDividerPadding(10);
+            ((LinearLayout) root).setDividerDrawable(drawable);
+        }
         MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(adapter);
         tabs.setupWithViewPager(viewPager);
+
     }
 
     @Override
@@ -46,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements DashboardFragment
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.searchAction:
                 Toast.makeText(this, "Will Perform Search", Toast.LENGTH_SHORT).show();
                 break;
